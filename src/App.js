d541aa19a9fbc6f1abaef2f83681c9cf31fc0ace
Caroline SENES <caro67200@gmail.com>
2022-08-07 22:59:31 +0200

    pass to in parent local state & add todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import TodoList from './components/TodoList';
-import AddTodo from './components/AddTodo';
+import { useState } from "react";
+import TodoList from "./components/TodoList";
+import AddTodo from "./components/AddTodo";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(content) {
+    //crypto.randomUUID() : génère un id unique de manière native
+    const todo = { id: crypto.randomUUID(), done: false, edit: false, content };
+    setTodoList([...todoList, todo]);
+    //met à jour le tableau "toodoList" et ajoute une nouvelle "todo" avec ses propriétés
+  }
+
   return (
     <div className="d-flex justify-content-center align-items-center p-20">
       <div className="card container p-20">
         <h1 className="mb-20">Liste de tâches</h1>
-        <AddTodo />
+        <AddTodo addTodo={addTodo} />
         <TodoList />
       </div>
     </div>
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,3 +1,39 @@
-export default function AddTodo() {
-    return <h2>Add Todo</h2>;
+import { useState } from "react";
+
+export default function AddTodo({ addTodo }) {
+  const [todo, setTodo] = useState("");
+
+  function handleChange(e) {
+    const inputValue = e.target.value;
+    setTodo(inputValue);
   }
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter" && todo.length) {
+      handleClick();
+    }
+  }
+
+  function handleClick() {
+    if (todo.length) {
+      addTodo(todo); //on appelle le setter du parent ("TodoList") passé en props
+      setTodo("");
+    }
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center mb-20">
+      <input
+        type="text"
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        value={todo}
+        className="mr-15 flex-fill"
+        placeholder="Ajouter une tâche"
+      />
+      <button onClick={handleClick} className="btn btn-primary">
+        Ajouter
+      </button>
+    </div>
+  );
+}
